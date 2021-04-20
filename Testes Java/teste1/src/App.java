e979c588494230842ae355414571d978c4ca4c8c
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-04-19 22:21:49 -0300

    outros testes

---
+++ Testes Java/teste1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String corno = "Fonti";
+        int i = 10;
+        System.out.println(String.format("Meu amigo %s eh corninho, ele foi corno %d vezes", corno, i));
+        System.out.println("Quem eh o animal marinho?????? " + tubarao());
+    }
+    public static String tubarao() {
+        return "Bruce";
+    }
+}
