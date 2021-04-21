d6eeb2c560f5e006cd45b5d2e39721b9efd24933
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-21 00:01:04 -0300

    finalização das atv 1 e 2 da aula 3

--- Programação Estruturada/Aula 3/Atv2/src/App.java
+++ Programação Estruturada/Aula 3/Atv2/src/App.java
@@ -2,9 +2,31 @@ import java.util.Scanner; //Importa o scanner ao Java
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Digite seu nome!");
-        Scanner leitor = new Scanner(System.in); //Instancia e inicializa um leitor de dados do teclado
+        Scanner leitor = new Scanner(System.in, "UTF-8");
+        
+        //Q1
+        /* System.out.println("Digite seu nome!");
         String nome = leitor.nextLine();
         System.out.println(String.format("Entao seu nome eh %s\nOla %s", nome, nome));
+         */
+
+        //Q2
+        /* String f1 = leitor.nextLine();
+        String f2 = leitor.nextLine();
+        String f3 = leitor.nextLine();
+        System.out.println(f3 + "\n" + f2 + "\n" + f1); */
+
+        //Q3
+        String[] info = leitor.nextLine().split(" ");
+        String nome = info[0];
+        String disciplina = info[1];
+        int n1 = Integer.parseInt(info[2]);
+        int n2 = Integer.parseInt(info[3]);
+        int n3 = Integer.parseInt(info[4]);
+        int n4 = Integer.parseInt(info[5]);
+
+        int media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println(String.format("O aluno %s, ficou com %d de media na disciplina de %s", nome, media, disciplina));
+
     }
 }
