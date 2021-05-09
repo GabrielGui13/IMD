5ee1760ef42071407680379ecec3a9dc1b621801
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-05-09 14:09:00 -0300

    aula 12

---
+++ ProgramaçãoEstruturada/Aula 12/Atv1/Q1/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+public class Carro {
+    public String marca;
+    public String modelo;
+    public int ano;
+    public int qtdP;
+}
+public class Pessoa { 
+	public String nome; 
+	public String telefone; 
+	public Carro carro;
+}
---
+++ ProgramaçãoEstruturada/Aula 12/Atv2/Q1/src/App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira o filme");
+        Filme f1 = registrarFilme();
+
+        System.out.println(f1);
+    }
+    public static Filme registrarFilme() {
+        Scanner leitor = new Scanner(System.in);
+        Filme f = new Filme();
+
+        f.nome = leitor.nextLine();
+        f.autor = leitor.nextLine();
+        f.ano = leitor.nextInt();
+        f.preco = leitor.nextDouble();
+
+        return f;
+    }
+}
+class Filme {
+    public String nome;
+    public String autor;
+    public int ano;
+    public double preco;
+
+    @Override
+    public String toString() {
+        return String.format("Nome = %s \nAutor = %s \nAno = %d, \nPreco = %f", nome, autor, ano, preco);
+    }
+}
